0103ef1048c5f7db0ea722719e82bd6ae442db36
Jaden Bertino <jaden@bertinofamily.com>
2022-11-21 11:46:07 -0800

    clean out filler code + setup index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import 'css/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React>
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// console.log(root);
+// root.render(
+//   <React>
+//     <App />
+//   </React>
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </React>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
