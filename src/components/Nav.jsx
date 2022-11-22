da5b7dc0254735092d5029cd0c5cfc19a17b1134
Jaden Bertino <jaden@bertinofamily.com>
2022-11-22 13:12:10 -0800

    align items center nav list + popup in progress

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -28,18 +28,20 @@ export default function Nav() {
               Books
             </Link>
           </li>
-          <li className="nav__list--item btn__menu--wrapper">
+          <li className="btn__menu--wrapper">
             <button className="btn__menu" onClick={ toggleMenu }>
               <FontAwesomeIcon icon={ faBars } className="btn__menu--icon" />
             </button>
           </li>
-          <li className="nav__list--item nav__icon">
+          <li className="nav__icon">
             <Link to="/cart" className="nav__list--link">
               <FontAwesomeIcon icon={ faCartShopping } className="nav__cart-icon"/>
             </Link>
           </li>
         </ul>
 
+        <div className="popup--backdrop"></div>
+        <div className="popup"></div>
       </div>
     </nav>
   )
