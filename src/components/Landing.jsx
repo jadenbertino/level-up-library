4b36b6cb7b5913b9adbacb852fd5e9ce3f507bda
Jaden Bertino <jaden@bertinofamily.com>
2022-11-23 10:50:39 -0800

    create landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,15 +6,13 @@ export default function Landing() {
       <header>
         <div className="header__container">
           <div className="header__description">
-            <h1>America's most awarded online library platform</h1>
-            <h2>Find your dream book with <span className="text--purple">Library</span></h2>
+            <h1 className="header__title">America's most awarded online library platform</h1>
+            <h2 className="header__sub-title">Find your dream book with <span className="text--purple">Library</span></h2>
             <a href="features">
               <button className="btn">Browse Books</button>
             </a>
           </div>
-          <figure className="header__img--wrapper">
-            <img src={ require("assets/undraw-books.svg").default } alt="" />
-          </figure>
+          <img src={ require("assets/undraw-books.svg").default } alt="" className="header__img" />
         </div>
       </header>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import './Home.css';
-import Landing from '../components/Landing'
+import Landing from 'components/Landing'
 import Highlights from '../components/Highlights'
 import Features from '../components/Featured'
 import CheapestBooks from '../components/CheapestBooks'
@@ -7,6 +7,6 @@ import Explore from '../components/Explore'
 
 export default function Home() {
   return (
-    <p>Home</p>
+    <Landing />
   )
 }
