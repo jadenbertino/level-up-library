4a91d63e27ffac313185113e71ef2dd98e4cc478
Jaden Bertino <jaden@bertinofamily.com>
2022-11-24 01:32:01 -0800

    make book component dynamic

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,14 +1,17 @@
 import { Rating, Price } from 'components/components'
+import React, { useState, useEffect, useRef }  from 'react';
+import { Link } from 'react-router-dom'
 
-export default function Book(book) {
+export default function Book({book}) {
+  console.log(book)
   return(
     <div className="book">
       <figure className="book__img--wrapper">
-        <img src={ require('assets/featured-1.png')} alt="" className="book__img" />
+        <img src={book.url} alt="" className="book__img" />
       </figure>
-      <h3 className="book__title">Cracking The Coding Interview</h3>
+      <h3 className="book__title">{book.title}</h3>
       <Rating rating={4.5} />
-      <Price originalPrice={59.95} salePrice={14.95} />
+      <Price originalPrice={book.originalPrice} salePrice={14.95} />
     </div>
   )
 }
