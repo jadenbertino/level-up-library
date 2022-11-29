cac5e297ed0b620f750560c0c7c5e036cf34cda7
Jaden Bertino <jaden@bertinofamily.com>
2022-11-29 15:46:43 -0800

    format book pricing component

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,13 +1,28 @@
-export default function Price({originalPrice, salePrice}) {
+export default function Price({ originalPrice, salePrice }) {
+  function formatPrice(price) {
+    if (Number.isInteger(price)) {
+      return price;
+    } else if (Number) {
+      return (Math.round(price * 100) / 100).toFixed(2);
+    } else {
+      return null;
+    }
+  }
+  const originalPriceFormatted = formatPrice(originalPrice);
+  const salePriceFormatted = formatPrice(salePrice);
+  // Display
   return (
     <div className="book__price">
-      { salePrice
-      ? <>
-        <span className="book__price--strikethrough">${ originalPrice }</span>
-        <span>${ salePrice }</span>
-      </>
-      : <span>${ originalPrice }</span>
-      }
+      { salePrice ? (
+        <>
+          <span className="book__price--strikethrough">
+            ${originalPriceFormatted}
+          </span>
+          <span>${salePriceFormatted}</span>
+        </>
+      ) : (
+        <span>${originalPriceFormatted}</span>
+      )}
     </div>
   );
 }
