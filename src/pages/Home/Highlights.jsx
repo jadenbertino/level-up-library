092d5d046f206581814fa49cab5c8c9a2fa7d32f
Jaden Bertino <jaden@bertinofamily.com>
2022-11-29 13:10:45 -0800

    update highlights section on home + implement sale price logic on book component

--- src/pages/Home/Highlights.jsx
+++ src/pages/Home/Highlights.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt } from "@fortawesome/free-solid-svg-icons";
+import { faBolt, faBookOpen, faTags } from "@fortawesome/free-solid-svg-icons";
 
 export default function Highlights() {
   return (
@@ -21,20 +21,20 @@ export default function Highlights() {
             </div>
             <div className="highlight">
               <div className="highlight__icon--wrapper">
-                <FontAwesomeIcon icon={faBolt} className="highlight__icon" />
+                <FontAwesomeIcon icon={faBookOpen} className="highlight__icon" />
               </div>
-              <h3 className="highlight__title">Easy And Quick</h3>
+              <h3 className="highlight__title">10,000+ Books</h3>
               <p className="highlight__para">
-                Get access to the book you purchased online instantly.
+                Library has books in all your favorite categories.
               </p>
             </div>
             <div className="highlight">
               <div className="highlight__icon--wrapper">
-                <FontAwesomeIcon icon={faBolt} className="highlight__icon" />
+                <FontAwesomeIcon icon={faTags} className="highlight__icon" />
               </div>
-              <h3 className="highlight__title">Easy And Quick</h3>
+              <h3 className="highlight__title">Affordable</h3>
               <p className="highlight__para">
-                Get access to the book you purchased online instantly.
+                Get your hands on popular books for as little as $10.
               </p>
             </div>
           </div>
