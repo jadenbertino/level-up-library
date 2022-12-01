0109941f6d7de0457c65dcb0bfe648e0ca8b2750
Jaden Bertino <jaden@bertinofamily.com>
2022-12-01 09:10:36 -0800

    implement useLocalStorage hook to preserve cart content upon page reload and browser close

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Home, Books, BookInfo } from "./pages/pages";
-import { Nav, Footer } from "./components/components";
+import { Nav, Footer, useLocalStorage } from "./components/components";
 import { bookData } from "assets/data";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 export default function App() {
   /* ------------------ CART ------------------ */
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useLocalStorage('cartLocalStorage', []);
 
   function addItemToCart(book) {
     const dupeItem = cart.find((item) => item.id === book.id); // returns dupe object or null
--- src/components/components.js
+++ src/components/components.js
@@ -4,5 +4,6 @@ import Book from "./Book.jsx";
 import Price from "./Price";
 import BestBooks from "./BestBooks";
 import Footer from "./Footer"
+import useLocalStorage from "./useLocalStorageHook.jsx";
 
-export { Nav, Rating, Book, Price, BestBooks, Footer };
+export { Nav, Rating, Book, Price, BestBooks, Footer, useLocalStorage };
