b6ae835aafa45ff95106888c266a6af414dd851d
Jaden Bertino <jaden@bertinofamily.com>
2023-05-04 14:34:26 -0700

    move cart functions into separate useCart hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,16 @@
-import useLocalStorage from 'hooks/useLocalStorage';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Footer, Nav } from './components/components';
 import { BookInfo, Books, Cart, Home } from './pages/pages';
+import useCart from 'hooks/useCart';
 
 // styles
 import './App.css';
 
 export default function App() {
-  const [cart, setCart] = useLocalStorage('cartLocalStorage', []);
-
-  function addItemToCart(book) {
-    const dupeItem = cart.find((item) => item.id === book.id); // returns dupe object or null
-    setCart((oldCart) =>
-      dupeItem
-        ? // item already in cart -> add 1 to quantity
-          oldCart.map((item) =>
-            item.id === dupeItem.id ? { ...item, quantity: item.quantity + 1 } : item
-          )
-        : // item not in cart -> set quantity to 1
-          [...oldCart, { ...book, quantity: 1 }]
-    );
-  }
-
-  function updateCart(item, newQuantity) {
-    setCart((oldCart) =>
-      oldCart.map((oldItem) =>
-        oldItem.id === item.id ? { ...oldItem, quantity: +newQuantity } : oldItem
-      )
-    );
-  }
-
-  function removeItem(item) {
-    setCart((oldCart) => oldCart.filter((cartItem) => cartItem.id !== item.id));
-  }
-
-  function getNumBooks() {
-    let numBooks = 0;
-    cart.forEach((item) => (numBooks += item.quantity));
-    return numBooks;
-  }
-
-  function calcPrices() {
-    let total = 0;
-    cart.forEach((item) => {
-      total += (item.salePrice || item.originalPrice) * item.quantity;
-    });
-    return {
-      subtotal: total * 0.9,
-      tax: total * 0.1,
-      total,
-    };
-  }
+  const { cart, addItemToCart, updateCart, removeItem, calcPrices, getNumBooks } = useCart()
 
   return (
-    <>
+    <div className='app'>
       <Router>
         <Nav numBooks={getNumBooks()} />
         <Routes>
@@ -74,6 +31,6 @@ export default function App() {
         </Routes>
         <Footer />
       </Router>
-    </>
+    </div>
   );
 }
---
+++ src/hooks/useCart.js
@@ -0,0 +1,50 @@
+import useLocalStorage from 'hooks/useLocalStorage';
+
+export default function useCart() {
+  const [cart, setCart] = useLocalStorage('cartLocalStorage', []);
+
+  function addItemToCart(book) {
+    const dupeItem = cart.find((item) => item.id === book.id); // returns dupe object or null
+    setCart((oldCart) =>
+      dupeItem
+        ? // item already in cart -> add 1 to quantity
+          oldCart.map((item) =>
+            item.id === dupeItem.id ? { ...item, quantity: item.quantity + 1 } : item
+          )
+        : // item not in cart -> set quantity to 1
+          [...oldCart, { ...book, quantity: 1 }]
+    );
+  }
+  
+  function updateCart(item, newQuantity) {
+    setCart((oldCart) =>
+      oldCart.map((oldItem) =>
+        oldItem.id === item.id ? { ...oldItem, quantity: +newQuantity } : oldItem
+      )
+    );
+  }
+  
+  function removeItem(item) {
+    setCart((oldCart) => oldCart.filter((cartItem) => cartItem.id !== item.id));
+  }
+  
+  function getNumBooks() {
+    let numBooks = 0;
+    cart.forEach((item) => (numBooks += item.quantity));
+    return numBooks;
+  }
+  
+  function calcPrices() {
+    let total = 0;
+    cart.forEach((item) => {
+      total += (item.salePrice || item.originalPrice) * item.quantity;
+    });
+    return {
+      subtotal: total * 0.9,
+      tax: total * 0.1,
+      total,
+    };
+  }
+  
+  return { cart, setCart, addItemToCart, updateCart, removeItem, getNumBooks, calcPrices}
+}
