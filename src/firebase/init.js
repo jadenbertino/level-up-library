ef39b309d617987376d217b47d14206815894d71
Jaden Bertino <jaden@bertinofamily.com>
2023-05-03 22:22:31 -0700

    change project name to 'level-up-library'

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATZpXegZ-WdbbUQGmIL-WHL0W63xbF_m8",
-  authDomain: "library-ecommerce.firebaseapp.com",
-  projectId: "library-ecommerce",
-  storageBucket: "library-ecommerce.appspot.com",
-  messagingSenderId: "286773940007",
-  appId: "1:286773940007:web:7cdd660f0a46e178206302"
+  apiKey: "AIzaSyCYbM66-YciqJLdkvOOldSu4I-sqojevpo",
+  authDomain: "level-up-library.firebaseapp.com",
+  projectId: "level-up-library",
+  storageBucket: "level-up-library.appspot.com",
+  messagingSenderId: "646446274520",
+  appId: "1:646446274520:web:4487be13f563b042fb84ad"
 };
 
 const app = initializeApp(firebaseConfig);
