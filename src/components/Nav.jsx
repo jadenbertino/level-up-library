764bf4ed15e72a04fb3c23c5edc31db09ebf0218
Jaden Bertino <jaden@bertinofamily.com>
2023-05-05 14:34:21 -0700

    fix nav mobile menu + swap fontawesome imports for sdk

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,6 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
 // styles & assets
-import { faBars, faCartShopping, faXmark } from '@fortawesome/free-solid-svg-icons';
 import logo from 'assets/library-logo.png';
 import './Nav.css';
 
@@ -30,31 +28,34 @@ export default function Nav({ numBooks }) {
           </li>
           <li className='btn__menu--wrapper'>
             <button className='btn__menu' onClick={toggleMenu}>
-              <FontAwesomeIcon icon={faBars} className='btn__menu--icon' />
+              <i className="fa-solid fa-bars btn__menu--icon"></i>
             </button>
           </li>
-          <li className='nav__icon'>
+          <li className='shopping-cart'>
             <Link to='/cart' className='nav__list--link'>
-              <FontAwesomeIcon icon={faCartShopping} className='nav__cart-icon' />
+              <i className='fa-solid fa-cart-shopping nav__cart-icon'></i>
               {numBooks > 0 ? <span className='cart__length'>{numBooks}</span> : null}
             </Link>
           </li>
         </ul>
-        <div className='popup'>
-          <FontAwesomeIcon icon={faXmark} className='popup__x-icon' onClick={toggleMenu} />
-          <ul className='popup__menu'>
-            <li className='popup__menu-item'>
-              <a href='#about' className='popup__menu-link' onClick={toggleMenu}>
-                About
-              </a>
+        <div className='modal'>
+          <i className="fa-solid fa-x modal__x-icon" onClick={toggleMenu}></i>
+          <ul className='modal__menu'>
+            <li className='modal__menu-item'>
+              <i className='fa-solid fa-house'></i>
+              <Link to='/' className='modal__menu-link' onClick={toggleMenu}>
+                Home
+              </Link>
             </li>
-            <li className='popup__menu-item'>
-              <a href='#books' className='popup__menu-link' onClick={toggleMenu}>
+            <li className='modal__menu-item'>
+              <i className='fa-solid fa-book'></i>
+              <Link to='/books' className='modal__menu-link' onClick={toggleMenu}>
                 Books
-              </a>
+              </Link>
             </li>
-            <li className='popup__menu-item'>
-              <Link to='/cart' className='popup__menu-link' onClick={toggleMenu}>
+            <li className='modal__menu-item'>
+              <i className='fa-solid fa-cart-shopping'></i>
+              <Link to='/cart' className='modal__menu-link' onClick={toggleMenu}>
                 Cart
               </Link>
             </li>
