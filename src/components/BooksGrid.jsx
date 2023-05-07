0b16487fb696ea57903aaef0917c441f15e128a4
Jaden Bertino <jaden@bertinofamily.com>
2023-05-07 15:05:58 -0700

    align books grid left bottom on desktop

--- src/components/BooksGrid.jsx
+++ src/components/BooksGrid.jsx
@@ -1,14 +1,14 @@
-import './BooksGrid.css'
-import Book from './Book'
+import Book from './Book';
+import './BooksGrid.css';
 
 export default function BooksGrid({ books }) {
   return (
-    <div className='books__container'>
+    <div className='books-grid'>
       {books.map((book) => (
         <div className='book__wrapper' key={book.id}>
           <Book book={book} />
         </div>
       ))}
     </div>
-  )
+  );
 }
