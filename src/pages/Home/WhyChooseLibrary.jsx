c76fd7d499fd6b71b57d5b2bddb2e5192fd0110d
Jaden Bertino <jaden@bertinofamily.com>
2023-05-07 09:15:18 -0700

    refactor 'why choose us' section on home into display: grid

--- src/pages/Home/WhyChooseLibrary.jsx
+++ src/pages/Home/WhyChooseLibrary.jsx
@@ -1,7 +1,7 @@
 // styles
-import './WhyChooseLibrary.css'
 import { faBolt, faBookOpen, faTags } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './WhyChooseLibrary.css';
 
 export default function FeatureShowcase() {
   return (
@@ -14,31 +14,31 @@ export default function FeatureShowcase() {
             </h2>
           </div>
         </div>
-        <div className='row highlights'>
-          <div className='col highlight'>
-            <div className='highlight__icon--wrapper'>
-              <FontAwesomeIcon icon={faBolt} className='highlight__icon' />
+        <div className='row'>
+          <div className='col'>
+            <div className='highlights'>
+              <div className='easy highlight__icon--wrapper'>
+                <FontAwesomeIcon icon={faBolt} className='highlight__icon' />
+              </div>
+              <h3 className='easy highlight__title'>Easy And Quick</h3>
+              <p className='easy highlight__para'>
+                Get access to the book you purchased online instantly.
+              </p>
+              <div className='access highlight__icon--wrapper'>
+                <FontAwesomeIcon icon={faBookOpen} className='highlight__icon' />
+              </div>
+              <h3 className='access highlight__title'>10,000+ Books</h3>
+              <p className='access highlight__para'>
+                Library has books in all your favorite categories.
+              </p>
+              <div className='affordable highlight__icon--wrapper'>
+                <FontAwesomeIcon icon={faTags} className='highlight__icon' />
+              </div>
+              <h3 className='affordable highlight__title'>Affordable</h3>
+              <p className='affordable highlight__para'>
+                Get your hands on popular books for as little as $10.
+              </p>
             </div>
-            <h3 className='highlight__title'>Easy And Quick</h3>
-            <p className='highlight__para'>
-              Get access to the book you purchased online instantly.
-            </p>
-          </div>
-          <div className='col highlight'>
-            <div className='highlight__icon--wrapper'>
-              <FontAwesomeIcon icon={faBookOpen} className='highlight__icon' />
-            </div>
-            <h3 className='highlight__title'>10,000+ Books</h3>
-            <p className='highlight__para'>Library has books in all your favorite categories.</p>
-          </div>
-          <div className='col highlight'>
-            <div className='highlight__icon--wrapper'>
-              <FontAwesomeIcon icon={faTags} className='highlight__icon' />
-            </div>
-            <h3 className='highlight__title'>Affordable</h3>
-            <p className='highlight__para'>
-              Get your hands on popular books for as little as $10.
-            </p>
           </div>
         </div>
       </div>
