3b6411dda150fce79dd678268026c649906ba15a
Jaden Bertino <jaden@bertinofamily.com>
2023-05-07 09:26:30 -0700

    implement container row col classes into landing and mobile optimize

--- src/pages/Home/Landing.jsx
+++ src/pages/Home/Landing.jsx
@@ -7,20 +7,22 @@ import './Landing.css';
 export default function Landing() {
   return (
     <section id='landing'>
-      <header>
-        <div className='container text--center'>
-          <div className='header__description'>
-            <h1 className='header__title'>America's most awarded online library platform</h1>
-            <h2 className='header__sub-title'>
-              Find your dream book with <span className='text--purple'>Library</span>
-            </h2>
-            <Link to='/books'>
-              <button className='btn'>Browse Books Now</button>
-            </Link>
+      <div className='container'>
+        <div className='row'>
+          <div className='col text--center'>
+            <div className='header__description'>
+              <h1 className='header__title'>America's most awarded online library platform</h1>
+              <h2 className='header__sub-title'>
+                Find your dream book with <span className='text--purple'>Library</span>
+              </h2>
+              <Link to='/books'>
+                <button className='btn'>Browse Books Now</button>
+              </Link>
+            </div>
+            <img src={landingImg} alt='' className='header__img' />
           </div>
-          <img src={landingImg} alt='' className='header__img' />
         </div>
-      </header>
+      </div>
     </section>
   );
 }
