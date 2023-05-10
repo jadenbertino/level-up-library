a5cbc6bb18be32c2c0fc8df5d1a3d803cd22af54
Jaden Bertino <jaden@bertinofamily.com>
2023-05-10 09:59:17 -0700

    simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,14 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-/**
- * Compiles SCSS files to CSS.
- * @returns {Stream} A stream of processed files.
- */
 function buildSass() {
-  return src('./src/sass/**/*.scss')
+  return src('./src/scss/**/*.scss')
     .pipe(sass())
     .pipe(dest('./src/css'));
 }
 
-/**
- * Watches SCSS files for changes and compiles them to CSS.
- * @param {Function} done - Callback function to signal the completion of the task.
- */
-function watchSass(done) {
-  buildSass().on('end', () => {
-    watch('./src/sass/**/*.scss', buildSass);
-    done();
-  });
-}
+const _watchSass = () => watch('./src/scss/**/*.scss', buildSass);
+const watchSass = series(buildSass, _watchSass);
 
 // Gulp task registration
 exports.buildSass = buildSass;
