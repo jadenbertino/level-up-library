9ca31ec3a49a44e2528ff19405362df19d04dc96
Jaden Bertino <jaden@bertinofamily.com>
2023-05-21 19:50:28 -0700

    footer - add link to logo and scrollToTop on all links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,36 @@
 import { Link } from 'react-router-dom';
+import { animateScroll as scroll } from 'react-scroll';
 
 // styles
 import logo from '../assets/library-logo.png';
 import '../css/components/Footer.css'
 
 export default function Footer() {
+
+  function scrollToTop() {
+    scroll.scrollToTop({ duration: 500 });
+  }
+
   return (
     <section id='footer'>
       <div className='container'>
         <div className='row'>
           <div className='col'>
-            <img src={logo} alt='' className='footer__logo' />
+            <Link to='/' onClick={scrollToTop}>
+              <img src={logo} alt='' className='footer__logo' />
+            </Link>
           </div>
         </div>
         <div className='row'>
           <div className='col'>
             <div className='footer__links'>
-              <Link to='/' className='footer__link'>
+              <Link to='/' className='footer__link' onClick={scrollToTop}>
                 Home
               </Link>
-              <Link to='/books' className='footer__link'>
+              <Link to='/books' className='footer__link'  onClick={scrollToTop}>
                 Books
               </Link>
-              <Link to='/cart' className='footer__link no-cursor'>
+              <Link to='/cart' className='footer__link'  onClick={scrollToTop}>
                 Cart
               </Link>
             </div>
